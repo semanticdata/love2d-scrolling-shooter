7bdf4f08946c2974d3e6d461b48dee7a0d817ebf
~data <108536517+datastring@users.noreply.github.com>
2023-07-02 19:00:13 -0500

    package ready

--- conf.lua
+++ conf.lua
@@ -1,10 +1,9 @@
 -- Configuration
 function love.conf(t)
-    t.title = "Scrolling Shooter Tutorial" -- The title of the window the game is in (string)
-    t.version = "11.4"                     -- The LÖVE version this game was made for (string)
-    t.window.width = 480                   -- we want our game to be long and thin.
+    t.title = "Scrolling Shooter" -- The title of the window the game is in (string)
+    t.version = "11.4"            -- The LÖVE version this game was made for (string)
+    t.window.width = 480          -- we want our game to be long and thin.
     t.window.height = 800
-
     -- For Windows debugging
-    t.console = true
+    -- t.console = false
 end
--- main.lua
+++ main.lua
@@ -1,38 +1,37 @@
-debug = true
+-- debug = false
 
 -- Timers
--- We declare these here so we don't have to edit them multiple places
-canShoot = true
-canShootTimerMax = 0.2 
-canShootTimer = canShootTimerMax
+canShoot            = true
+canShootTimerMax    = 0.2
+canShootTimer       = canShootTimerMax
 createEnemyTimerMax = 0.4
-createEnemyTimer = createEnemyTimerMax
+createEnemyTimer    = createEnemyTimerMax
 
 -- Player Object
-player = { x = 200, y = 710, speed = 150, img = nil }
-isAlive = true
-score  = 0
+player              = { x = 200, y = 710, speed = 150, img = nil }
+isAlive             = true
+score               = 0
 
 -- Image Storage
-bulletImg = nil
-enemyImg = nil
+bulletImg           = nil
+enemyImg            = nil
 
 -- Sound Storage
-gunSound = nil
+gunSound            = nil
 
 -- Entity Storage
-bullets = {} -- array of current bullets being drawn and updated
-enemies = {} -- array of current enemies on screen
+bullets             = {} -- array of current bullets being drawn and updated
+enemies             = {} -- array of current enemies on screen
 
 -- Collision detection taken function from http://love2d.org/wiki/BoundingBox.lua
 -- Returns true if two boxes overlap, false if they don't
 -- x1,y1 are the left-top coords of the first box, while w1,h1 are its width and height
 -- x2,y2,w2 & h2 are the same, but for the second box
-function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
-  return x1 < x2+w2 and
-         x2 < x1+w1 and
-         y1 < y2+h2 and
-         y2 < y1+h1
+function CheckCollision(x1, y1, w1, h1, x2, y2, w2, h2)
+	return x1 < x2 + w2 and
+		x2 < x1 + w1 and
+		y1 < y2 + h2 and
+		y2 < y1 + h1
 end
 
 -- Loading
@@ -42,7 +41,6 @@ function love.load(arg)
 	bulletImg = love.graphics.newImage('assets/bullet.png')
 	gunSound = love.audio.newSource("assets/gun-sound.wav", "static")
 end
-
 
 -- Updating
 function love.update(dt)
@@ -99,38 +97,38 @@ function love.update(dt)
 			end
 		end
 
-		if CheckCollision(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(), player.x, player.y, player.img:getWidth(), player.img:getHeight()) 
-		and isAlive then
+		if CheckCollision(enemy.x, enemy.y, enemy.img:getWidth(), enemy.img:getHeight(), player.x, player.y, player.img:getWidth(), player.img:getHeight())
+			and isAlive then
 			table.remove(enemies, i)
 			isAlive = false
 		end
 	end
 
 	-- Horizontal movement
-	if love.keyboard.isDown('left','a') then
+	if love.keyboard.isDown('left', 'a') then
 		if player.x > 0 then -- binds us to the map
-			player.x = player.x - (player.speed*dt)
+			player.x = player.x - (player.speed * dt)
 		end
-	elseif love.keyboard.isDown('right','d') then
+	elseif love.keyboard.isDown('right', 'd') then
 		if player.x < (love.graphics.getWidth() - player.img:getWidth()) then
-			player.x = player.x + (player.speed*dt)
+			player.x = player.x + (player.speed * dt)
 		end
 	end
 
 	-- Vertical movement
 	if love.keyboard.isDown('up', 'w') then
 		if player.y > (love.graphics.getHeight() / 2) then
-			player.y = player.y - (player.speed*dt)
+			player.y = player.y - (player.speed * dt)
 		end
 	elseif love.keyboard.isDown('down', 's') then
 		if player.y < (love.graphics.getHeight() - 55) then
-			player.y = player.y + (player.speed*dt)
+			player.y = player.y + (player.speed * dt)
 		end
 	end
 
 	if love.keyboard.isDown('space', 'rctrl', 'lctrl') and canShoot then
 		-- Create some bullets
-		newBullet = { x = player.x + (player.img:getWidth()/2), y = player.y, img = bulletImg }
+		newBullet = { x = player.x + (player.img:getWidth() / 2), y = player.y, img = bulletImg }
 		table.insert(bullets, newBullet)
 		gunSound:play()
 		canShoot = false
@@ -172,11 +170,11 @@ function love.draw(dt)
 	if isAlive then
 		love.graphics.draw(player.img, player.x, player.y)
 	else
-		love.graphics.print("Press 'R' to restart", love.graphics:getWidth()/2-50, love.graphics:getHeight()/2-10)
+		love.graphics.print("Press 'R' to restart", love.graphics:getWidth() / 2 - 50, love.graphics:getHeight() / 2 - 10)
 	end
 
 	if debug then
 		fps = tostring(love.timer.getFPS())
-		love.graphics.print("Current FPS: "..fps, 9, 10)
+		love.graphics.print("Current FPS: " .. fps, 9, 10)
 	end
 end
